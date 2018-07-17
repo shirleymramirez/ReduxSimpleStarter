2a51a133d37b40a32aa0ccd530c51d7ac7eb8817
shirley <shirley.mramirez@yahoo.com>
2018-07-17 10:56:06 -0700

    added mapDispatchToProps

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,13 +3,14 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
 	constructor(props){
 		super(props);
 
 		this.state = { term: ''};
 
 		this.onInputChange = this.onInputChange.bind(this);
+		this.onFormSubmit = this.onFormSubmit.bind(this);
 	}
 
 	onInputChange(event){
@@ -21,7 +22,8 @@ export default class SearchBar extends Component {
 
 
 		//fetch weather data
-
+		this.props.fetchWeather(this.state.term);
+		this.setState({ term: ''});
 	}
 
 
@@ -40,3 +42,9 @@ export default class SearchBar extends Component {
 		);
 	}
 }
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
