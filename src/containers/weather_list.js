ea0d8cafcff9a58a48764720063f51fbd98f564f
shirley <shirley.mramirez@yahoo.com>
2018-07-18 16:24:24 -0700

    added sparkline charts

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,23 @@
 import React, {  Component } from 'react';
-
 import { connect} from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
+
 
 class WeatherList extends Component {
 	renderWeather(cityData) {
+		console.log(cityData);
 		const name = cityData.city.name;
+		const temps = cityData.list.map(weather => weather.main.temp);
+		console.log(temps);
+
 		return (
 			<tr key={name}>
 				<td>{name}</td>
+				<td>
+					<Sparklines height={120} width={180} data={temps}>
+						<SparklinesLine color="blue" />
+					</Sparklines>
+				</td>
 			</tr>	
 		);
 
